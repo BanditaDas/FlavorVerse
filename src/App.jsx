ed6b8ab46870b4816ce5d7202107d042f3bcda75
Bandita Das <dbandita362@gmail.com>
2026-06-18 10:01:30 +0530

    start from the start

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Mainroutes from './Routes/Mainroutes'
 
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen bg-orange-50 text-gray-800 font-sans selection:bg-orange-200 selection:text-orange-900">
-      <Navbar />
-      <Mainroutes />
-    </div>
+    <div>App</div>
   )
 }
+
+export default App
--- src/Routes/Mainroutes.jsx
+++ src/Routes/Mainroutes.jsx
@@ -1,26 +1,13 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Home from '../components/Home'
-import Product, { products } from '../components/Product'
-import Service from '../components/Service'
-import About from '../components/About'
-import ProdDets from '../components/ProdDets'
-import SerDets from '../components/SerDets'
 
 function Mainroutes() {
-    return (
-        <main className="container mx-auto px-4 py-8">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/product" element={<Product />} />
-                <Route path="/product/:id" element={<ProdDets products={products} />} />
-                <Route path="/service" element={<Service />} >
-                    <Route path="/service/details" element={<SerDets />} />
-                </Route>
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </main>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/recipe" element={<Recipe />} />
+    </Routes>
+  )
 }
 
 export default Mainroutes

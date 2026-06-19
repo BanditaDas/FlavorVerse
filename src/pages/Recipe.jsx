89b33d2f145bcb09c700eb12304429517c11c65a
Bandita Das <dbandita362@gmail.com>
2026-06-19 11:24:09 +0530

    recipe container added

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Recipecard from '../components/Recipecard';
 
 function Recipe() {
+  const recipe = {
+    name: 'Spaghetti Carbonara',
+    imageUrl: 'https://images.unsplash.com/photo-1621996346565-e326b20f5413?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  };
+
   return (
-    <div>Recipe</div>
+    <div>
+      <Recipecard recipe={recipe} />
+    </div>
   )
 }
 
